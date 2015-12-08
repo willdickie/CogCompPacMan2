b84df84889699e4ef2a0d2a087c9964e137ec82f
William Dickie <willbdickie@gmail.com>
2015-12-08 17:38:57 -0500

    created semi-GA

--- pacman-ai.js
+++ pacman-ai.js
@@ -22,7 +22,50 @@ var PACAI =  (function (Pacman) {
 	function virtualKeyRaw(char) {
 		return {keyCode:char};
 	}
-	
+
+	function selectSuper(scores, weights) {
+		var tempArray = scores;
+		tempArray.sort(sortFunction);
+		var super1 = tempArray[0];
+		var super2 = tempArray[1];
+		var newGen = createNewGeneration(super1,super2);
+		reRun(newGen);
+	}
+	function sortFunction(a, b) {
+	    if (a[0] === b[0]) {
+	        return 0;
+	    }
+	    else {
+	        return (a[0] < b[0]) ? -1 : 1;
+	    }
+	}
+	function createNewGeneration(super1, super2){
+		
+		var newGeneration = [];
+		for(int i = 0; i<5;i++){
+			newGeneration.append(createMutation(super1));
+		}
+		for(int i = 0; i<5;i++){
+			newGeneration.append(createMutation(super1))
+
+		}
+		return newGeneration;
+	}
+	function createMutation(super){
+		var mutationRate = 4.2;
+		var rand = Math.Random(0,100);
+		var temp[];
+		temp[0] = 0;
+		temp[1] = []
+		for(int i = 0; i < 3;i++){
+				if(rand < mutationRate){
+					temp[1][i] = super[1][i] + super[1][i] * Math.Random(-.1,.1);
+				}else{
+					temp[1][i] = suer[1][i]
+				}
+		}
+		return temp;
+	}
 	window.onNewMap = function (map, userPos, ghosts) {		
 		for(var i=0;i<ghosts.length;i++){
 			console.log(ghosts[i].isVunerable());
